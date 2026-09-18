c63b63aa61e7c230f556a9888f5bbc6489bb42d5
kyratna <kyashoratna@gmail.com>
2026-09-18 21:43:21 +0530

    fix(integrations): update Google Apps Script deployment URL

--- src/content.js
+++ src/content.js
@@ -352,7 +352,7 @@ const content = {
   integrations: {
     // Google Sheets & Drive integration (Primary)
     appsScriptUrl:
-      "https://script.google.com/macros/s/AKfycbxibdTIQ5m8jfUdoCZ2SSAIVNOIJE6uzDPKrPUulREgq8rZ8TCtykTghNK7WJWR86oxxA/exec",
+      "https://script.google.com/macros/s/AKfycbylOxWOQ0hVZfrTDVLmqm3qIhdIRylwc-kOgu0iu-zrTkQeutSD96UARBdNltJe1zMDlA/exec",
 
     // Firebase Firestore integration (Real-time text blessings & RSVPs)
     firebase: {
